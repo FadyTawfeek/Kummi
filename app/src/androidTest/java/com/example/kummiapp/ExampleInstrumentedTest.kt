fe06fc889e972255a6f5f764c7696c867ccdae72
Adnan1706 <islam.adnan07@yahoo.com>
2020-04-26 12:34:52 +0300

    Development Completed

--- app/src/androidTest/java/com/example/kummiapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/kummiapp/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.kummiapp", appContext.packageName)
+        assertEquals("com.example.kummi", appContext.packageName)
     }
 }
